2eabf0970dee13588432e5030005582a2c217508
Yasaman <yasaman.rohani@gmail.com>
2018-12-22 14:52:54 -0500

    rotate, swirl, transform anim files

--- app/src/main/java/com/example/yasamanro/bhangarigame/MainActivity.java
+++ app/src/main/java/com/example/yasamanro/bhangarigame/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.yasamanro.bhangarigame;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +37,19 @@ public class MainActivity extends AppCompatActivity {
                 customDialog("How To Play", "1. Choose a Tool!\n2. Extract an electronic part using your tool\n3. Test your part!\n4. Decide whether to reuse or recycle!");
             }
         });
-    }
 
+
+
+        // Bluetooth stuff
+        BluetoothAdapter mBluetoothAdapter;
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);           // Initialize Bluetooth adapter.
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+        // Ensures Bluetooth is available on the device and it is enabled. If not, displays a dialog requesting user permission to enable Bluetooth.
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
 
     public void customDialog(String title, String message){
         final android.support.v7.app.AlertDialog.Builder builderSingle = new android.support.v7.app.AlertDialog.Builder(this, R.style.MyDialogTheme);
